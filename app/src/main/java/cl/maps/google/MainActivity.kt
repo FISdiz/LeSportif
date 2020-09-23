7e21a136d34e8cd5fdc45c316be69ec8138c4174
foxhunter <kevin.floba@gmail.com>
2020-09-23 12:31:38 -0300

    Se crea una aplicación con MainActivity de 4 buttons, un fragment de maps y los colores

---
+++ app/src/main/java/cl/maps/google/MainActivity.kt
@@ -0,0 +1,15 @@
+package cl.maps.google
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
